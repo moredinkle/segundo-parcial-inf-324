e633919789732f039b552c0e46492f92458d2f29
moredinkle <44622066+moredinkle@users.noreply.github.com>
2021-12-06 03:45:42 -0400

    Corregido

--- Ejercicio3/WindowsFormsApplication21/WindowsFormsApplication21/Form1.cs
+++ Ejercicio3/WindowsFormsApplication21/WindowsFormsApplication21/Form1.cs
@@ -32,7 +32,7 @@ namespace WindowsFormsApplication21
         {
             Bitmap bmp = new Bitmap(pictureBox1.Image);
             Color c = new Color();
-            c = bmp.GetPixel(10,10);
+            c = bmp.GetPixel(10, 10);
             textBox1.Text = c.R.ToString();
             //textBox2.Text = c.G.ToString();
             textBox3.Text = c.B.ToString();
@@ -51,9 +51,9 @@ namespace WindowsFormsApplication21
             cB = c.B;*/
             Bitmap bmp = new Bitmap(pictureBox1.Image);
             Color c = new Color();
-            int x, y, mR=0, mG=0, mB=0;
+            int x, y, mR = 0, mG = 0, mB = 0;
             x = e.X; y = e.Y;
-            for (int i = x; i < x + 10;i++)
+            for (int i = x; i < x + 10; i++)
                 for (int j = y; j < y + 10; j++)
                 {
                     c = bmp.GetPixel(i, j);
@@ -95,7 +95,7 @@ namespace WindowsFormsApplication21
                 for (int j = 1; j < bmp.Height; j++)
                 {
                     c = bmp.GetPixel(i, j);
-                    cpoa.SetPixel(i, j, Color.FromArgb(c.R, 0 , 0) );
+                    cpoa.SetPixel(i, j, Color.FromArgb(c.R, 0, 0));
                 }
             pictureBox2.Image = cpoa;
         }
@@ -125,7 +125,7 @@ namespace WindowsFormsApplication21
                     c = bmp.GetPixel(i, j);
                     if (((cR - 10) < c.R) && (c.R < (cR + 10)) && ((cG - 10) < c.G) && (c.G < (cG + 10)) && ((cB - 10) < c.B) && (c.B < (cB + 10)))
                         cpoa.SetPixel(i, j, Color.Black);
-                    else 
+                    else
                         cpoa.SetPixel(i, j, c);
                 }
             pictureBox2.Image = cpoa;
@@ -137,32 +137,33 @@ namespace WindowsFormsApplication21
             Bitmap bmp = new Bitmap(pictureBox1.Image);
             Bitmap cpoa = new Bitmap(bmp.Width, bmp.Height);
             Color c = new Color();
-            for (int i = 0; i < bmp.Width-10; i+=10)
-                for (int j = 0; j < bmp.Height-10; j+=10)
+            for (int i = 0; i < bmp.Width - 10; i += 10)
+                for (int j = 0; j < bmp.Height - 10; j += 10)
                 {
                     meR = 0;
                     meG = 0;
                     meB = 0;
 
-                    for (int k = i; k < i+10; k++)
+                    for (int k = i; k < i + 10; k++)
                         for (int l = j; l < j + 10; l++)
                         {
                             c = bmp.GetPixel(k, l);
-                            meR = meR+c.R;
-                            meG = meG+c.G;
-                            meB = meB+c.B;
+                            meR = meR + c.R;
+                            meG = meG + c.G;
+                            meB = meB + c.B;
                         }
-                    meR = meR /100;
-                    meG = meG /100;
-                    meB = meB /100;
-                    
+                    meR = meR / 100;
+                    meG = meG / 100;
+                    meB = meB / 100;
+
                     if (((cR - 10) < meR) && (meR < (cR + 10)) && ((cG - 10) < meG) && (meG < (cG + 10)) && ((cB - 10) < meB) && (meB < (cB + 10)))
-                        for (int k = i; k < i+10; k++)
+                        for (int k = i; k < i + 10; k++)
                             for (int l = j; l < j + 10; l++)
                             {
                                 cpoa.SetPixel(k, l, Color.Black);
                             }
-                        
+                    
+
                     else
                         for (int k = i; k < i + 10; k++)
                             for (int l = j; l < j + 10; l++)
@@ -199,13 +200,13 @@ namespace WindowsFormsApplication21
         {
             SqlConnection con = new SqlConnection("Data Source=LAPTOP-Q81M2BTO\\SQLEXPRESS;Initial Catalog=imagenes;Persist Security Info=True;User ID=user324;Password=324");
             con.Open();
-            String com = "insert into textura(t1r, t1g, t1b, t2r, t2g, t2b, t3r, t3g, t3b) values('"+t1r+"','"+t1g+"','"+t1b+"','"+t2r+"','"+t2g+"','"+t2b+"','"+t3r+"','"+t3g+"','"+t3b +"');";
+            String com = "insert into textura(t1r, t1g, t1b, t2r, t2g, t2b, t3r, t3g, t3b) values('" + t1r + "','" + t1g + "','" + t1b + "','" + t2r + "','" + t2g + "','" + t2b + "','" + t3r + "','" + t3g + "','" + t3b + "');";
             SqlCommand cmd = new SqlCommand(com, con);
             int i = cmd.ExecuteNonQuery();
             if (i != 0)
             {
                 MessageBox.Show("Datos guardados");
-                cambiarColor();
+                cambiarColorv2();
             }
             else MessageBox.Show("Error");
         }
@@ -233,5 +234,64 @@ namespace WindowsFormsApplication21
             textBox4.Text = "Al final";
             pictureBox2.Image = cpoa;
         }
+
+        public void cambiarColorv2()
+        {
+            int meR, meG, meB;
+            cR = t1r; cG = t1g; cB = t1b; cR2 = t2r; cG2 = t2g; cB2 = t2b; cR3 = t3r; cG3 = t3g; cB3 = t3b;
+            Bitmap bmp = new Bitmap(pictureBox1.Image);
+            Bitmap cpoa = new Bitmap(bmp.Width, bmp.Height);
+            Color c = new Color();
+            for (int i = 0; i < bmp.Width - 10; i += 10)
+                for (int j = 0; j < bmp.Height - 10; j += 10)
+                {
+                    meR = 0;
+                    meG = 0;
+                    meB = 0;
+
+                    for (int k = i; k < i + 10; k++)
+                        for (int l = j; l < j + 10; l++)
+                        {
+                            c = bmp.GetPixel(k, l);
+                            meR = meR + c.R;
+                            meG = meG + c.G;
+                            meB = meB + c.B;
+                        }
+                    meR = meR / 100;
+                    meG = meG / 100;
+                    meB = meB / 100;
+
+                    if (((cR - 10) < meR) && (meR < (cR + 10)) && ((cG - 10) < meG) && (meG < (cG + 10)) && ((cB - 10) < meB) && (meB < (cB + 10)))
+                        for (int k = i; k < i + 10; k++)
+                            for (int l = j; l < j + 10; l++)
+                            {
+                                cpoa.SetPixel(k, l, Color.Black);
+                            }
+
+                    else if (((cR2 - 10) < meR) && (meR < (cR2 + 10)) && ((cG2 - 10) < meG) && (meG < (cG2 + 10)) && ((cB2 - 10) < meB) && (meB < (cB2 + 10)))
+                        for (int k = i; k < i + 10; k++)
+                            for (int l = j; l < j + 10; l++)
+                            {
+                                cpoa.SetPixel(k, l, Color.ForestGreen);
+                            }
+
+                    else if (((cR3 - 10) < meR) && (meR < (cR3 + 10)) && ((cG3 - 10) < meG) && (meG < (cG3 + 10)) && ((cB3 - 10) < meB) && (meB < (cB3 + 10)))
+                        for (int k = i; k < i + 10; k++)
+                            for (int l = j; l < j + 10; l++)
+                            {
+                                cpoa.SetPixel(k, l, Color.Yellow);
+                            }
+
+                    else
+                        for (int k = i; k < i + 10; k++)
+                            for (int l = j; l < j + 10; l++)
+                            {
+                                c = bmp.GetPixel(k, l);
+                                cpoa.SetPixel(k, l, c);
+                            }
+                }
+            pictureBox2.Image = cpoa;
+        }
+
     }
 }
